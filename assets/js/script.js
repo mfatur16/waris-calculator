30ee1481433a9a8a3b7186b8919949638a7b173b
mfatur16 <72298468+mfatur16@users.noreply.github.com>
2023-01-11 21:22:55 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// function anakYa() {
+//     document.getElementById("alak").disabled = false;
+//     document.getElementById("aper").disabled = false;
+// }
+// function anakTidak() {
+//     document.getElementById("alak").disabled = true;
+//     document.getElementById("aper").disabled = true;
+// }
+
+// $('#harta').priceFormat({
+//     prefix: 'Rp.',
+//     centsLimit: 0,
+//     thousandsSeparator: '.'
+// });
+
+function anakYa() {
+    document.getElementById('jumlahAnak').hidden = false;
+}
+
+function anakTidak() {
+    document.getElementById('jumlahAnak').hidden = true;
+}
+
+// function anak() {
+//     if (document.getElementById('anak').value == 'Ya') {
+//         document.getElementById('jumlahAnak').hidden = false;
+//     }
+//     else {
+//         document.getElementById('jumlahAnak').hidden = true;
+//     }
+// }
